5615d87f074ae44e7802b16692f3dd40034ce9fa
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2019-09-16 15:32:41 +0300

    add viewColumnWithEndDate to default settings for options

--- daterangepicker.js
+++ daterangepicker.js
@@ -55,6 +55,7 @@
         this.autoUpdateInput = true;
         this.alwaysShowCalendars = false;
         this.ranges = {};
+        this.viewColumnWithEndDate = false;
 
         this.opens = 'right';
         if (this.element.hasClass('pull-right'))
@@ -554,7 +555,13 @@
                 if (!this.linkedCalendars && (this.endDate.month() != this.startDate.month() || this.endDate.year() != this.startDate.year())) {
                     this.rightCalendar.month = this.endDate.clone().date(2);
                 } else {
-                    this.rightCalendar.month = this.startDate.clone().date(2).add(1, 'month');
+                    if (viewColumnWithEndDate) {
+                        this.leftCalendar.month = this.endDate.clone().date(2).subtract(1, 'month');;
+                        this.rightCalendar.month = this.endDate.clone().date(2);
+                    }
+                    else {
+                        this.rightCalendar.month = this.startDate.clone().date(2).add(1, 'month');
+                    }
                 }
 
             } else {
